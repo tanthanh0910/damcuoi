f039d4fcc2e6be9d7eb4aedc255309f6b870b91f
ThanhLe0910 <tanthanhle091@gmail.com>
2022-12-07 14:40:16 +0700

    update file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>@yield('title',config('app.name'))</title>
+        <base href="{{asset('')}}">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
         <title>Laravel</title>
 
